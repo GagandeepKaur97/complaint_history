528b072a836e3bfa86f671713395fe1dca2a0c5d
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-02 23:17:27 -0400

    [gk]]design modified

--- app/src/main/java/com/gagan/complaint_history/MainActivity.java
+++ app/src/main/java/com/gagan/complaint_history/MainActivity.java
@@ -3,8 +3,16 @@ package com.gagan.complaint_history;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
 
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btnAdd);
 
-}}
+        final ArrayList<String> list = new ArrayList<>();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }}
